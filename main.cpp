26aad14f70006c223e1d6679557cbdee7fcd841f
kimurareo <k024g0034@g.neec.ac.jp>
2025-04-25 17:33:18 +0900

    a

--- main.cpp
+++ main.cpp
@@ -3,11 +3,120 @@
 
 const char kWindowTitle[] = "LC1D_10_キムラ_レオ_タイトル";
 
+// 4x4行列の構造体
+struct Matrix4x4 {
+	float m[4][4];
+};
+
+struct Vector3
+{
+	float x;
+	float y;
+	float z;
+};
 
 
 //==============================================================
 // 関数の作成
 //==============================================================
+
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
+
+	Matrix4x4 result = {};
+	result.m[0][0] = 1;
+	result.m[0][1] = 0;
+	result.m[0][2] = 0;
+	result.m[0][3] = 0;
+	result.m[1][0] = 0;
+	result.m[1][1] = 1;
+	result.m[1][2] = 0;
+	result.m[1][3] = 0;
+	result.m[2][0] = 0;
+	result.m[2][1] = 0;
+	result.m[2][2] = 1;
+	result.m[2][3] = 0;
+	result.m[3][0] = translate.x;
+	result.m[3][1] = translate.y;
+	result.m[3][2] = translate.z;
+	result.m[3][3] = 1;
+
+		return result;
+
+};
+
+Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
+	Matrix4x4 result = {};
+	result.m[0][0] = scale.x;
+	result.m[0][1] = 0;
+	result.m[0][2] = 0;
+	result.m[0][3] = 0;
+	result.m[1][0] = 0;
+	result.m[1][1] = scale.y;
+	result.m[1][2] = 0;
+	result.m[1][3] = 0;
+	result.m[2][0] = 0;
+	result.m[2][1] = 0;
+	result.m[2][2] = scale.x;
+	result.m[2][3] = 0;
+	result.m[3][0] = 0;
+	result.m[3][1] = 0;
+	result.m[3][2] = 0;
+	result.m[3][3] = 1;
+
+	return result;
+
+
+};
+
+Vector3 Transform(const Matrix4x4& matrix,const Vector3& vector) {
+
+	Vector3 result = {};
+	result.x = matrix.m[0][0] * vector.x + matrix.m[1][0] * vector.y + matrix.m[2][0] * vector.z + matrix.m[3][0];
+	result.y = matrix.m[0][1] * vector.x + matrix.m[1][1] * vector.y + matrix.m[2][1] * vector.z + matrix.m[3][1];
+	result.z = matrix.m[0][2] * vector.x + matrix.m[1][2] * vector.y + matrix.m[2][2] * vector.z + matrix.m[3][2];
+
+	float w = vector.x * matrix.m[0][3] + vector.y * matrix.m[1][3] + vector.z * matrix.m[2][3] + matrix.m[3][3];
+
+	if (w != 0) {
+		result.x /= w;
+		result.y/= w;
+		result.z /= w;
+	}
+
+	return result;
+
+
+};
+
+static const int kColumWidth = 60;
+static const int kRowHeight = 20;
+
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumWidth, y, "%.2f", vector.y);
+	Novice::ScreenPrintf(x + kColumWidth * 2, y, "%0.2f", vector.z);
+	Novice::ScreenPrintf(x + kColumWidth * 3, y, "%s", label);
+}
+
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
+
+	Novice::ScreenPrintf(x, y, "%s", label);
+
+	for (int row = 0; row < 4; ++row) {
+
+		for (int column = 0; column < 4; ++column) {
+
+			Novice::ScreenPrintf(
+				x + column * kColumWidth, (y + row * kRowHeight) + 20, "%6.02f", matrix.m[row][column]
+			);
+
+		}
+
+	}
+
+}
+
 
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -23,9 +132,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//==============================================================
 	// 宣言と初期化
 	//==============================================================
-
+	Vector3 translate{ 4.1f,2.6f,0.8f };
+	Vector3 scale{ 1.5f,5.2f,7.3f };
+	Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
+	Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
+	Vector3 point{ 2.3f,3.8f,1.4f };
+	Matrix4x4 transformMatrix = {
+		1.0f,2.0f,3.0f,4.0f,
+		3.0f,1.0f,1.0f,2.0f,
+		1.0f,4.0f,2.0f,3.0f,
+		2.0f,2.0f,1.0f,3.0f
+	};
 	
-
+	Vector3 transformed = Transform(transformMatrix, point);
 
 
 	// ウィンドウの×ボタンが押されるまでループ
@@ -53,8 +172,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		// 計算した数値を関数で表示する
 		
-
-
+		
+		VectorScreenPrintf(0, 0, transformed, "transformed");
+		MatrixScreenPrintf(0, kRowHeight * 5, translateMatrix, "translateMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 10, scaleMatrix, "scaleMatrix");
 
 
 		///
