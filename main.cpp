228f8f708b70b8188172b26b19a03075a9a1c649
kimurareo <k024g0034@g.neec.ac.jp>
2025-04-25 16:48:04 +0900

    a

--- main.cpp
+++ main.cpp
@@ -3,96 +3,12 @@
 
 const char kWindowTitle[] = "LC1D_10_キムラ_レオ_タイトル";
 
-struct Vector3
-{
-	float x;
-	float y;
-	float z;
-};
 
 
 //==============================================================
 // 関数の作成
 //==============================================================
 
-// 加算
-Vector3 Add(const Vector3& v1, const Vector3& v2) {
-
-	Vector3 result = {};
-
-	result.x = v1.x + v2.x;
-	result.y = v1.y+ v2.y;
-	result.z = v1.z + v2.z;
-
-	return result;
-};
-
-// 減算
-Vector3 Subtract(const Vector3& v1, const Vector3& v2) {
-
-	Vector3 result = {};
-
-	result.x = v1.x - v2.x;
-	result.y = v1.y - v2.y;
-	result.z = v1.z - v2.z;
-
-	return result;
-};
-
-// スカラー倍
-Vector3 Multiply(float scalar, const Vector3& v2) {
-
-	Vector3 result = {};
-
-	result.x = scalar * v2.x;
-	result.y = scalar * v2.y;
-	result.z = scalar * v2.z;
-
-	return result;
-};
-
-// 内積
-float Dot(const Vector3& v1, const Vector3& v2) {
-
-	float result = {};
-
-	result = v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
-	
-
-	return result;
-};
-
-// 長さ
-float Length(const Vector3& v) {
-
-	float result = {};
-
-	result = sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
-
-	return result;
-
-};
-
-// 正規化
-Vector3 Normalize(const Vector3& v) {
-
-	Vector3 result{};
-
-	result.x = v.x / sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
-	result.y = v.y / sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
-	result.z = v.z / sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
-
-	return result;
-};
-
-static const int kColumWidth = 60;
-
-void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
-	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
-	Novice::ScreenPrintf(x + kColumWidth, y, "%.2f", vector.y);
-	Novice::ScreenPrintf(x + kColumWidth * 2, y, "%0.2f", vector.z);
-	Novice::ScreenPrintf(x + kColumWidth * 3, y, "%s", label);
-}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -108,9 +24,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// 宣言と初期化
 	//==============================================================
 
-	Vector3 v1{ 1.0f, 3.0f,-5.0f };
-	Vector3 v2{ 4.0f,-1.0f,2.0f };
-	float k = { 4.0f };
+	
 
 
 
@@ -128,13 +42,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 
 		// 作った関数に数値を代入する
-		Vector3 resultAdd = Add(v1, v2);
-		Vector3 resultSubtract = Subtract(v1, v2);
-		Vector3 resultMultiply = Multiply(k, v1);
-		float resultDot = Dot(v1, v2);
-		float resultLength = Length(v1);
-		Vector3 resultNormalize = Normalize(v2);
-
+		
 		///
 		/// ↑更新処理ここまで
 		///
@@ -144,12 +52,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 
 		// 計算した数値を関数で表示する
-		VectorScreenPrintf(0, 0, resultAdd, "  : Add");
-		VectorScreenPrintf(0, kColumWidth, resultSubtract, "  : Subtract");
-		VectorScreenPrintf(0, kColumWidth * 2, resultMultiply, "  : Multiply");
-		Novice::ScreenPrintf(0, kColumWidth * 3, "%.2f  : Dot", resultDot);
-		Novice::ScreenPrintf(0, kColumWidth * 4, "%.2f  : Length", resultLength);
-		VectorScreenPrintf(0, kColumWidth * 5, resultNormalize, "  : Normalize");
+		
 
 
 
